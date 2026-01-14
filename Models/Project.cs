a89e3e8af7d197f375ca6aaaf284c9581af7b557
Adarsh Sudarsanan <adarshsudarsan573@gmail.com>
2026-01-14 13:42:42 +0530

    Remove Required attributes from Project properties
    Removed data annotations from Project model properties.

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,26 +1,12 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace PortfolioAPI.Models
 {
     public class Project
     {
         public int Id { get; set; }
-
-        [Required]
         public string Title { get; set; } = string.Empty;
-
-        [Required]
         public string Description { get; set; } = string.Empty;
-
-        [Required]
         public string TechStack { get; set; } = string.Empty;
-
-        [Required]
         public string GithubUrl { get; set; } = string.Empty;
-
-        [Required]
         public string LiveUrl { get; set; } = string.Empty;
-
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
