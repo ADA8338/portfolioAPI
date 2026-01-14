be92108b554e8ccbc0dbe5f9ee6bd9f16583f316
Adarsh Sudarsanan <adarshsudarsan573@gmail.com>
2026-01-14 15:06:57 +0530

    Update PortfolioDbContext.cs

--- Data/PortfolioDbContext.cs
+++ Data/PortfolioDbContext.cs
@@ -6,7 +6,9 @@ namespace PortfolioAPI.Data
     public class PortfolioDbContext : DbContext
     {
         public PortfolioDbContext(DbContextOptions<PortfolioDbContext> options)
-            : base(options) { }
+            : base(options)
+        {
+        }
 
         public DbSet<Project> Projects { get; set; }
     }
