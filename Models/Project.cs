bb5aea2cbc01e7413a11e0cf7e3fbd74cb79a5a0
Adarsh Sudarsanan <adarshsudarsan573@gmail.com>
2026-01-14 14:48:43 +0530

    Update Project.cs

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,12 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PortfolioAPI.Models
 {
     public class Project
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; } = string.Empty;
+
         public string Description { get; set; } = string.Empty;
+
         public string TechStack { get; set; } = string.Empty;
-        public string GithubUrl { get; set; } = string.Empty;
-        public string LiveUrl { get; set; } = string.Empty;
+
+        public string ProjectUrl { get; set; } = string.Empty;
     }
 }
