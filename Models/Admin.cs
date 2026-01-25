a70531918a4474d4a8f287e82e5c9c90331804a2
Adarsh Sudarsanan <adarshsudarsan573@gmail.com>
2026-01-25 12:12:49 +0530

    Update Admin.cs

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -3,7 +3,7 @@ namespace PortfolioAPI.Models
     public class Admin
     {
         public int Id { get; set; }
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
     }
 }
